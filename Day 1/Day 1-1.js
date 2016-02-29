5d1482c4cb321e0331343b0a1c0c7360d2f8cc2a
Philip Giannioudis <giannioudis@gmail.com>
2016-02-29 13:30:25 -0500

    Day 1
    Tested Works

---
+++ Day 1/Day 1-1.js
@@ -0,0 +1,18 @@
+// complete the function
+function vowelsAndConsonants(s) {
+    
+    var Arr = s.toLowerCase().split( '' ),
+    c = '',
+    vArr = ['a','e','i','o','u'];
+
+    for ( c of Arr ){        
+        if( vArr.indexOf( c )!==-1 ){
+            console.log( c );
+        }
+    }
+    for ( c of Arr ){        
+        if( vArr.indexOf( c )===-1 ){
+            console.log( c );
+        }
+    }
+}
